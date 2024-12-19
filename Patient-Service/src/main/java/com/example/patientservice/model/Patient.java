8070b80bbba9dce1f2ed686f91ddf39b634bd6b4
silvanshine <theo.piacentini@gmail.com>
2024-12-19 09:58:50 +0100

    create patient crud for patient service

---
+++ Patient-Service/src/main/java/com/example/patientservice/model/Patient.java
@@ -0,0 +1,48 @@
+package com.example.patientservice.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.data.annotation.Id;
+
+@ApiModel(description = "Details about the patient")
+public class Patient {
+
+    private static int idCounter = 0;
+
+    @Id
+    @ApiModelProperty(notes = "The unique ID of the patient, auto-incremented")
+    private int id;
+
+    @ApiModelProperty(notes = "The name of the patient")
+    private String name;
+
+
+    public Patient(String name) {
+        this.id = ++idCounter;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
--- Patient-Service/src/test/java/com/example/patientservice/SpringBootSwagger2ApplicationTests.java
+++ Patient-Service/src/test/java/com/example/patientservice/SpringBootSwagger2ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.springbootswagger2;
+package com.example.patientservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringBootSwagger2ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
